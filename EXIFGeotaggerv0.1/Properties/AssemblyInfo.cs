15604768486577b4fe7d76a3c2d570f33d4bc721
mattwynyard <mjwynyard@gmail.com>
2019-04-15 14:52:51 +1200

    updated copyright

--- EXIFGeotaggerv0.1/Properties/AssemblyInfo.cs
+++ EXIFGeotaggerv0.1/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("EXIFGeotaggerv0.1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("Onsite Developments")]
 [assembly: AssemblyProduct("EXIFGeotaggerv0.1")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyCopyright("Copyright © Onsite Developments 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
